f14946f20b01b8c600e449f7b14a5dfac6035325
baoloc1993 <baoloc1993@gmail.com>
2015-03-23 21:40:50 +0800

    add model

--- cz3003/cms/admin.py
+++ cz3003/cms/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 from cms.models import Map
+from cms.models import GoogleMap
 from cms.models import CallOperatorForm
-from cms.models import ReportForm
+from cms.models import CrisisInstance
+from cms.models import SensorData
+from cms.models import Event 
+from cms.models import NEA
+from cms.models import Weather
+from cms.models import Event
+from cms.models import Account
+from cms.models import Report
+from cms.models import EmailMonitoring
+
+
 
 
 
 # Register your models here.
-admin.site.register(Map)
+admin.site.register(GoogleMap)
 admin.site.register(CallOperatorForm)
-admin.site.register(ReportForm)
+admin.site.register(CrisisInstance)
+admin.site.register(Event)
+admin.site.register(Account)
+admin.site.register(NEA)
+admin.site.register(Weather)
+admin.site.register(Report)
+admin.site.register(EmailMonitoring)
+
--- cz3003/cz3003/urls.py
+++ cz3003/cz3003/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from cms import views
+
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +10,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^home/', include('cms.urls')),
+
 )
